a9b3c9b7f0e96693f141c9667c806b45ff565f64
Vitalii Grygoruk <vitalii@peakon.com>
2019-04-11 12:28:09 +0200

    prettier

--- test/specs/sample.test.js
+++ test/specs/sample.test.js
@@ -1,6 +1,22 @@
 describe('my awesome website', () => {
   it('should do some chai assertions', () => {
-      browser.url('https://webdriver.io');
-      browser.getTitle().should.include('Next-gen WebDriver test framework for Node.js');
+    browser.url('https://webdriver.io');
+    browser
+      .getTitle()
+      .should.include('Next-gen WebDriver test framework for Node.js');
+  });
+
+  it('should do some chai assertions 2', () => {
+    browser.url('https://webdriver.io');
+    browser
+      .getTitle()
+      .should.include('Next-gen WebDriver test framework for Node.js');
+  });
+
+  it('should do some chai assertions 3', () => {
+    browser.url('https://webdriver.io');
+    browser
+      .getTitle()
+      .should.include('Next-gen WebDriver test framework for Node.js');
   });
 });
